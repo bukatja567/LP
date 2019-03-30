fef467030e66ccd840a444bce6b8aa8f371dcab7
bukatja567 <bukatja567@mail.ru>
2019-03-30 12:08:04 +0300

    New

---
+++ examle2.py
@@ -0,0 +1,20 @@
+#if-elif-else
+"""
+company = input("Введите слово: ")
+if "my" and "google" in company:
+    print("Условие выполнено!")
+elif "google" in company: 
+    print("Он нашел google")
+elif "my" in company:
+    print("Есть не все")
+else:
+    print("Неа, не пущу!")
+"""
+a=3
+b=8
+sum = 0
+i=3
+while i != 1:
+    sum = sum + a + b
+    i = i-1
+print(sum)
