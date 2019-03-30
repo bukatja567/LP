d8367f30fbe03a80bfb864d92d1a2cb8749aa9fd
bukatja567 <bukatja567@mail.ru>
2019-03-30 11:51:16 +0300

    New

--- example.py
+++ example.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+a = 2
+b = 3
+year = input("Введите год: ")
+year = int(year)
+is_leap = year % 4 == 0 and (year % 100 != 0 or year %400 ==0)
+print(is_leap)
+"""
+Привет, я твой комментарий. Давай дружить!
+"""
+str = "Hello"
+print(str[::-1])
+print(str[1::3])
+print("Hello" + "Мир")
+print("Мамаааа"*100)
+print(id(str))
