20e2e6de8ead8295debdd19221004dfe898bf312
bukatja567 <bukatja567@mail.ru>
2019-04-13 09:29:56 +0300

    RANDOM

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
+"""
 a = 2
 b = 3
 year = input("Введите год: ")
 year = int(year)
 is_leap = year % 4 == 0 and (year % 100 != 0 or year %400 ==0)
 print(is_leap)
+"""
 """
 Привет, я твой комментарий. Давай дружить!
 """
